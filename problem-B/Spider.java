5cfdeb55582d19e5cc45a2784921b1a1a389e0ec
zhaniyazhaniya <124429520+zhaniyazhaniya@users.noreply.github.com>
2023-02-26 02:20:00 +0600

    created Spider class that extends Animal1

---
+++ problem-B/Spider.java
@@ -0,0 +1,8 @@
+public class Spider extends Animal1 {
+    public Spider(){
+        super(8);
+    }
+    public void eat(){
+        System.out.println("Spiders eat insects or other small animals");
+    }
+}
